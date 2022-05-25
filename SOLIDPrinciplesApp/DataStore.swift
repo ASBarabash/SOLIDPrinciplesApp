0adeb61d8e3dec6c1286ac145678d2c2177b83c0
Alexander Barabash <alexanderbarabash86@gmail.com>
2022-05-25 18:15:31 +0700

    SOLIDPrinciples

---
+++ SOLIDPrinciplesApp/DataStore.swift
@@ -0,0 +1,21 @@
+//
+//  DataStore.swift
+//  SOLIDPrinciplesApp
+//
+//  Created by Alexandr Barabash on 25.05.2022.
+//
+
+import Foundation
+
+class DataStore {
+    
+    // Сохранение данных в кеш/базу данных
+    func savenameInCache(name: String) {
+        print("Your name: \(name) is saved")
+    }
+    
+    // Получать данные из кеша/базы данных
+    func getNameFromCache() -> String {
+        return "some name"
+    }
+}
--- SOLIDPrinciplesApp/ViewController.swift
+++ SOLIDPrinciplesApp/ViewController.swift
@@ -7,13 +7,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Country: Decodable {
+    var Id: String
+    var Time: String
+    var Name: String
+    var Image: String?
+}
 
+class ViewController: UIViewController {
+    let urlString = "https://raw.githubusercontent.com/Softex-Group/task-mobile/master/test.json"
+    
+    // Внешние зависимости
+    var networkService = NetworkService()
+    let dataStore = DataStore()
+    
+    
+    // Элементы пользовательского интерфейса
+    @IBOutlet var myTextField: UITextField!
+    @IBOutlet var textLabel: UILabel!
+    @IBOutlet var saveButton: UIButton!
+    
+    // MARK: - View lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        saveButton.layer.cornerRadius = saveButton.frame.width / 2
+
+        networkService.dataFetcher(urlString: urlString)
     }
-
-
+    
+    // MARK: - Business logic
+    
+    // Обработка имени
+    func changeName() {
+        guard let name = textLabel.text, name != "" else {
+            showAlert()
+            return
+        }
+        
+        dataStore.savenameInCache(name: name)
+    }
+    
+    //MARK: - User interface
+    
+    // Отображение интерфейса
+    func showAlert() {
+        let alert = UIAlertController(title: "WARNING", message: "Your name can't be empty.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Okey", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    // по которому и происходит вся работа
+    // Взаимодействие пользователя с интерфейсом
+    @IBAction func changeLabel() {
+        textLabel.text = myTextField.text
+    }
+    
+    @IBAction func saveButtonTapped() {
+    }
 }
 
